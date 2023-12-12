3e21e522f94200f893522012db8b4ffc6f4c2392
Abdul.Ghafoor <Abdul.Ghafoor@whetstonez.com>
2023-12-12 22:17:37 +0500

    adding files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webappmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
